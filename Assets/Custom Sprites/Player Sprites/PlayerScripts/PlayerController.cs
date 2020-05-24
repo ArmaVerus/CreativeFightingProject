986ca4c4c3709d2d80fca7ea53102cb674ebc589
ArmaVerus <65874168+ArmaVerus@users.noreply.github.com>
2020-05-24 23:48:59 +0100

    Uploading Base Project

---
+++ Assets/Custom Sprites/Player Sprites/PlayerScripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        velocity = moveInput * speed;
+    }
+}
