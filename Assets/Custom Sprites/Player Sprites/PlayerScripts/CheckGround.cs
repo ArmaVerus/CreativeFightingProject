653ef031f880dc2410001c71fa226e75231841db
ArmaVerus <65874168+ArmaVerus@users.noreply.github.com>
2020-05-26 02:23:51 +0100

    Created movement, Started Gravity Logic

---
+++ Assets/Custom Sprites/Player Sprites/PlayerScripts/CheckGround.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    GameObject playerDetail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDetail = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            playerDetail.GetComponent<PlayerController>().isOnFloor = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            playerDetail.GetComponent<PlayerController>().isOnFloor = false;
+        }
+    }
+}
--- Assets/Custom Sprites/Player Sprites/PlayerScripts/PlayerController.cs
+++ Assets/Custom Sprites/Player Sprites/PlayerScripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpSpeed;
     private Rigidbody2D rb;
     private Vector2 velocity;
+    public bool isOnFloor = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
         velocity = moveInput * speed;
+        
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        //rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        //if (counter < 50)
+        //{
+        //    counter += 1;
+        //}
+        //else
+        //{
+        //    Debug.Log("jump");
+        //    counter = 0;
+        //    rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        //}
+        Jump();
+        if (!isOnFloor)
+        {
+            rb.AddForce(new Vector2(0, -jumpSpeed/5), ForceMode2D.Impulse);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetAxis("Vertical") > 0 && isOnFloor == true)
+        {
+            Debug.Log("jump");
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+        
     }
 }
